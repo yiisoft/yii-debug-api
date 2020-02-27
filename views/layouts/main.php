bba29a9160a234d258ea8996301f4bcbc3187c7c
Dmitriy Derepko <xepozz@list.ru>
2020-02-27 22:53:53 +0300

    Fixed layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,27 +1,33 @@
 <?php
 
-use yii\helpers\Html;
+/**
+ * @var \Yiisoft\Router\UrlGeneratorInterface $urlGenerator
+ * @var \Yiisoft\View\WebView $this
+ * @var \Yiisoft\Assets\AssetManager $assetManager
+ * @var string $content
+ * @var null|string $currentUrl
+ */
 
-/* @var $this \yii\web\View */
-/* @var $content string */
+$this->setCssFiles($assetManager->getCssFiles());
+$this->setJsFiles($assetManager->getJsFiles());
 
-Yiisoft\Yii\Debug\Viewer\Asset\DebugAsset::register($this);
-?>
-<?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html>
+$this->beginPage();
+?><!DOCTYPE html>
+<html lang="">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="none" />
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode(\yii\helpers\Yii::getApp()->controller->module->htmlTitle()) ?></title>
+    <title>Yii Debug Viewer</title>
     <?php $this->head() ?>
 </head>
 <body>
-<?php $this->beginBody() ?>
-<?= $content ?>
-<?php $this->endBody() ?>
+<?php
+$this->beginBody();
+echo $content;
+$this->endBody();
+?>
 </body>
 </html>
-<?php $this->endPage() ?>
+<?php
+$this->endPage(true);
