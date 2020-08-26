da5bf6bc56591f715d34318f276d73463f4919f6
Rustam Mamadaminov <rmamdaminov@gmail.com>
2020-08-26 22:11:40 +0500

    Don't collect incoming requests

--- config/params.php
+++ config/params.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 return [
+    'yiisoft/yii-debug' => [
+        'optionalRequests' => ['/debug/*']
+    ],
     'yiisoft/yii-debug-api' => [
         'enabled' => true,
         'allowedIPs' => ['127.0.0.1', '::1'],
