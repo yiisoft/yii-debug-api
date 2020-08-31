99661f07b7953983533d3d7b04f01313999aef6d
Rustam Mamadaminov <rmamdaminov@gmail.com>
2020-08-31 13:09:08 +0500

    Adjustments (#8)
    * Adjust Repository
    
    * Make 'id' mandatory

--- src/Repository/CollectorRepository.php
+++ src/Repository/CollectorRepository.php
@@ -20,11 +20,7 @@ class CollectorRepository implements CollectorRepositoryInterface
     {
         $data = $this->loadData('.index.json', $id);
         if ($id !== null) {
-            if (isset($data[$id])) {
-                return $data[$id];
-            }
-
-            throw new NotFoundException(sprintf('Unable to find debug data ID with "%s"', $id));
+            return $data;
         }
 
         return array_values($data);
@@ -33,35 +29,31 @@ class CollectorRepository implements CollectorRepositoryInterface
     public function getDetail(string $id, ?string $collector = null): array
     {
         $data = $this->loadData('.data.json', $id);
-        if (!isset($data[$id])) {
-            throw new NotFoundException(sprintf('Unable to find debug data ID with "%s"', $id));
-        }
 
         if (empty($collector)) {
-            return $data[$id];
+            return $data;
         }
 
-        if (!isset($data[$id][$collector])) {
+        if (!isset($data[$collector])) {
             throw new NotFoundException(sprintf('Unable to find debug data collected with "%s"', $collector));
         }
 
-        return $data[$id][$collector];
+        return $data[$collector];
     }
 
     private function loadData(string $fileSuffix, ?string $id = null): array
     {
         clearstatcache();
-        $data = [];
         if (!empty($id)) {
             $file = $this->path . DIRECTORY_SEPARATOR . $id . $fileSuffix;
-            if (file_exists($file) && !is_dir($file)) {
-                $id = \basename($file, $fileSuffix);
-                $data[$id] = Json::decode(file_get_contents($file));
+            if (!file_exists($file) || is_dir($file)) {
+                throw new NotFoundException(sprintf('Unable to find debug data ID with "%s"', $id));
             }
 
-            return $data;
+            return Json::decode(file_get_contents($file));
         }
 
+        $data = [];
         $dataFiles = \glob($this->path . '/yii-debug*' . $fileSuffix, GLOB_NOSORT);
         foreach ($dataFiles as $file) {
             $id = \basename($file, $fileSuffix);
