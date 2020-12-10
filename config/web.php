0f456a97c74776d5c6c8e20ad95d7668da90ec76
Rustam Mamadaminov <rmamdaminov@gmail.com>
2020-12-10 07:23:21 +0500

    Don't access to debug files directly, use storage read instead.

--- config/web.php
+++ config/web.php
@@ -6,14 +6,12 @@ use Psr\Container\ContainerInterface;
 use Yiisoft\Aliases\Aliases;
 use Yiisoft\Yii\Debug\Api\Repository\CollectorRepository;
 use Yiisoft\Yii\Debug\Api\Repository\CollectorRepositoryInterface;
+use Yiisoft\Yii\Debug\Storage\StorageInterface;
 
 /**
  * @var $params array
  */
 
 return [
-    CollectorRepositoryInterface::class => static function (ContainerInterface $container) use ($params) {
-        $aliases = $container->get(Aliases::class);
-        return new CollectorRepository($aliases->get($params['yiisoft/yii-debug']['path']));
-    },
+    CollectorRepositoryInterface::class => static fn (StorageInterface $storage) => new CollectorRepository($storage)
 ];
