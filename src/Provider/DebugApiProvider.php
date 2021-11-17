2ac9df58a64841d0c0625b2a51b22538dea847df
Alexander Makarov <sam@rmcreative.ru>
2021-11-17 23:25:06 +0300

    Adjust to yiisoft/di changes

--- src/Provider/DebugApiProvider.php
+++ src/Provider/DebugApiProvider.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Yii\Debug\Api\Provider;
 
 use Psr\Container\ContainerInterface;
-use Yiisoft\Di\Contracts\ServiceProviderInterface;
+use Yiisoft\Di\ServiceProviderInterface;
 use Yiisoft\Yii\Debug\Api\Middleware\DebugHeaders;
 use Yiisoft\Router\RouteCollectorInterface;
 
