5b0b7669ff68260ac02d3b3ae6f22ab352d1d9fe
Dmitrii Derepko <xepozz@list.ru>
2022-11-21 21:46:14 +0300

    Add file explorer

--- src/Controller/InspectController.php
+++ src/Controller/InspectController.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\Debug\Api\Controller;
 
+use FilesystemIterator;
 use InvalidArgumentException;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use RecursiveDirectoryIterator;
 use ReflectionClass;
+use SplFileInfo;
 use Throwable;
+use Yiisoft\Aliases\Aliases;
 use Yiisoft\Config\ConfigInterface;
 use Yiisoft\DataResponse\DataResponseFactoryInterface;
 use Yiisoft\VarDumper\VarDumper;
@@ -45,6 +49,68 @@ class InspectController
         ksort($params);
 
         return $this->responseFactory->createResponse($params);
+    }
+
+    public function files(Aliases $aliases, ServerRequestInterface $request): ResponseInterface
+    {
+        $request = $request->getQueryParams();
+        $path = $request['path'] ?? '';
+
+        $rootPath = $aliases->get("@root");
+
+        $destination = realpath($rootPath . $path);
+
+        if (!file_exists($destination)) {
+            throw new InvalidArgumentException(sprintf('Destination "%s" does not exist', $destination));
+        }
+
+
+        if (!is_dir($destination)) {
+            $file = new SplFileInfo($destination);
+            return $this->responseFactory->createResponse([
+                'directory' => $this->removeBasePath($rootPath, dirname($destination)),
+                'content' => file_get_contents($destination),
+                'path' => $this->removeBasePath($rootPath, $destination),
+                'absolutePath' => $destination,
+                ...$this->serializeFileInfo($file),
+            ]);
+        }
+
+        /**
+         * @var $directoryIterator SplFileInfo[]
+         */
+        $directoryIterator = new RecursiveDirectoryIterator(
+            $destination,
+            FilesystemIterator::KEY_AS_PATHNAME | FilesystemIterator::CURRENT_AS_FILEINFO
+        );
+
+        $files = [];
+        foreach ($directoryIterator as $file) {
+            if ($file->getBasename() === '.') {
+                continue;
+            }
+
+            $path = $file->getPathName();
+            if ($file->isDir()) {
+                if ($file->getBasename() === '..') {
+                    $path = realpath($path);
+                }
+                $path .= '/';
+            }
+            /**
+             * Check if path is inside the application directory
+             */
+            if (!str_starts_with($path, $rootPath)) {
+                continue;
+            }
+            $path = $this->removeBasePath($rootPath, $path);
+            $files[] = [
+                'path' => $path,
+                ...$this->serializeFileInfo($file),
+            ];
+        }
+
+        return $this->responseFactory->createResponse($files);
     }
 
     public function classes(ContainerInterface $container): ResponseInterface
@@ -116,5 +182,55 @@ class InspectController
         $result = $container->get($map[$commandName])->run();
 
         return $this->responseFactory->createResponse($result);
+    }
+
+    private function removeBasePath(string $rootPath, string $path): string|array|null
+    {
+        return preg_replace(
+            '/^' . preg_quote($rootPath, '/') . '/',
+            '',
+            $path,
+            1
+        );
+    }
+
+    private function getUserOwner(int $uid): array
+    {
+        if ($uid ===0 || !function_exists('posix_getpwuid') || false === ($info = posix_getpwuid($uid))) {
+            return [
+                'id' => $uid
+            ];
+        }
+        return [
+            'uid' => $info['uid'],
+            'gid' => $info['gid'],
+            'name' => $info['name'],
+        ];
+    }
+
+    private function getGroupOwner(int $gid): array
+    {
+        if ($gid ===0 || !function_exists('posix_getgrgid') || false === ($info = posix_getgrgid($gid))) {
+            return [
+                'id' => $gid
+            ];
+        }
+        return [
+            'gid' => $info['gid'],
+            'name' => $info['name'],
+        ];
+    }
+
+    private function serializeFileInfo(SplFileInfo $file): array
+    {
+        return [
+            'baseName' => $file->getBasename(),
+            'extension' => $file->getExtension(),
+            'user' => $this->getUserOwner((int)$file->getOwner()),
+            'group' => $this->getGroupOwner((int)$file->getGroup()),
+            'size' => $file->getSize(),
+            'type' => $file->getType(),
+            'permissions' => substr(sprintf('%o', $file->getPerms()), -4),
+        ];
     }
 }
