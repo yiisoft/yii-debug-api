d0f3c862140a520b01b08dc8ad2eb630328cf5d6
Wilmer Arambula <terabytesoftw@gmail.com>
2023-10-27 10:08:57 -0300

    Replace `AR` with `Db`.

--- src/Inspector/Database/Db/DbSchemaProvider.php
+++ src/Inspector/Database/Db/DbSchemaProvider.php
@@ -2,42 +2,33 @@
 
 declare(strict_types=1);
 
-namespace Yiisoft\Yii\Debug\Api\Inspector\Database\ActiveRecord;
+namespace Yiisoft\Yii\Debug\Api\Inspector\Database\Db;
 
-use Yiisoft\ActiveRecord\ActiveRecord;
-use Yiisoft\ActiveRecord\ActiveRecordFactory;
 use Yiisoft\Db\Connection\ConnectionInterface;
+use Yiisoft\Db\Query\Query;
 use Yiisoft\Db\Schema\ColumnSchemaInterface;
 use Yiisoft\Db\Schema\TableSchemaInterface;
 use Yiisoft\Yii\Debug\Api\Inspector\Database\SchemaProviderInterface;
 
-class ActiveRecordSchemaProvider implements SchemaProviderInterface
+class DbSchemaProvider implements SchemaProviderInterface
 {
-    public function __construct(
-        private ConnectionInterface $connection,
-        private ActiveRecordFactory $activeRecordFactory,
-    ) {
+    public function __construct(private ConnectionInterface $db)
+    {
     }
 
     public function getTables(): array
     {
+        $quoter = $this->db->getQuoter();
         /** @var TableSchemaInterface[] $tableSchemas */
-        $tableSchemas = $this->connection->getSchema()->getTableSchemas();
-
+        $tableSchemas = $this->db->getSchema()->getTableSchemas();
         $tables = [];
+
         foreach ($tableSchemas as $schema) {
-            $activeQuery = $this->activeRecordFactory->createQueryTo(ActiveRecord::class, $schema->getName());
-
-            /**
-             * @var ActiveRecord[] $records
-             */
-            $records = $activeQuery->count();
-
             $tables[] = [
-                'table' => $schema->getName(),
+                'table' => $quoter->unquoteSimpleTableName($schema->getName()),
                 'primaryKeys' => $schema->getPrimaryKey(),
                 'columns' => $this->serializeARColumnsSchemas($schema->getColumns()),
-                'records' => $records,
+                'records' => (new Query($this->db))->from($schema->getName())->count(),
             ];
         }
         return $tables;
@@ -46,20 +37,14 @@ class ActiveRecordSchemaProvider implements SchemaProviderInterface
     public function getTable(string $tableName): array
     {
         /** @var TableSchemaInterface[] $tableSchemas */
-        $schema = $this->connection->getSchema()->getTableSchema($tableName);
-
-        $activeQuery = $this->activeRecordFactory->createQueryTo(ActiveRecord::class, $tableName);
-
-        /**
-         * @var ActiveRecord[] $records
-         */
-        $records = $activeQuery->all();
-
+        $schema = $this->db->getSchema()->getTableSchema($tableName);
+        $records = (new Query($this->db))->from($schema->getName())->all();
         $data = [];
+
         // TODO: add pagination
-        foreach ($records as $n => $record) {
-            foreach ($record->attributes() as $attribute) {
-                $data[$n][$attribute] = $record->{$attribute};
+        foreach ($records as $r => $record) {
+            foreach ($record as $n => $attribute) {
+                $data[$r][$n] = $attribute;
             }
         }
 
