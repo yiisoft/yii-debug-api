426f0b0c3da5ae09aebe48b9e7cc69902ef97e7d
Dmitrii Derepko <xepozz@list.ru>
2024-07-06 21:01:51 +0300

    Check if opcache is enabled

--- src/Inspector/Controller/OpcacheController.php
+++ src/Inspector/Controller/OpcacheController.php
@@ -6,8 +6,7 @@ namespace Yiisoft\Yii\Debug\Api\Inspector\Controller;
 
 use Psr\Http\Message\ResponseInterface;
 use Yiisoft\DataResponse\DataResponseFactoryInterface;
-
-use function Safe\opcache_get_status;
+use Yiisoft\Http\Status;
 
 final class OpcacheController
 {
@@ -18,9 +17,15 @@ final class OpcacheController
 
     public function index(): ResponseInterface
     {
+        if (!\function_exists('opcache_get_status') || ($status = \opcache_get_status(true)) === false) {
+            return $this->responseFactory->createResponse([
+                'message' => 'OPcache is not installed or configured',
+            ], Status::UNPROCESSABLE_ENTITY);
+        }
+
         return $this->responseFactory->createResponse([
-            'status' => opcache_get_status(true),
-            'configuration' => opcache_get_configuration(),
+            'status' => $status,
+            'configuration' => \opcache_get_configuration(),
         ]);
     }
 }
